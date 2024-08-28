0d0f2c5be41b987eb68a9fb33eacf5d333420fde
Allan Nava <allannava95@gmail.com>
2024-08-28 11:27:27 +0200

    added db lib class

---
+++ app/lib/database/db.go
@@ -0,0 +1,28 @@
+package database
+
+import (
+	"fmt"
+    "log"
+	"strconv"
+
+	"github.com/Allan-Nava/go-wire-fuego-scafffold/env"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+
+func NewDB(config *env.Configuration) *gorm.DB {
+    dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", config.DbUsername, config.TangramDbPassword, config.DbHost, config.DbPort, config.DbName)
+	conn, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("error connection on %s, err: %s", dsn, err.Error())
+	}
+	db, _ := conn.DB()
+	idle, _ := strconv.Atoi(config.DbIdleConn)
+	max, _ := strconv.Atoi(config.DbMaxConn)
+
+	db.SetMaxIdleConns(idle)
+	db.SetMaxOpenConns(max)
+
+	return conn
+}
--- env/config.go
+++ env/config.go
@@ -12,6 +12,13 @@ import (
 type Configuration struct {
 	AppEnv   string `env:"APP_ENV"`
 	LogLevel string `env:"LOG_LEVEL"`
+	DbUsername                string `env:"DB_USERNAME"`
+	DbPassword                string `env:"DB_PASSWORD"`
+	DbHost                    string `env:"DB_HOST"`
+	DbPort                    string `env:"DB_PORT"`
+	DbIdleConn                string `env:"DB_IDLE_CONN"`
+	DbMaxConn                 string `env:"DB_MAX_CONN"`
+	DbName                    string `env:"DB_NAME"`
 }
 
 func GetEnvConfig() *Configuration {
